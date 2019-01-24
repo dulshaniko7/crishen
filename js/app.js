0872afe2fd1048db1046389f36c9357856f8b51c
dulshaniko7 <45009976+dulshaniko7@users.noreply.github.com>
2019-01-24 13:50:11 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
-$(document).ready(function () {
-    $('.sidenav').sidenav();
-    $(".scrollspy").scrollSpy();
-});
+let myChart = document.getElementById('myChart').getContext('2d');
+
+let barChart = new Chart(myChart, {
+    type:'bar',
+    data:{
+        labels:['HTML','CSS','SASS','MATERIALIZE CSS','BOOTSTRAP','JAVA SCRIPT','JQUERY','ANGULAR 6'],
+        datasets :[{
+            label:'Skills',
+            data:[
+                95,
+                91,
+                67,
+                94,
+                87,
+                85,
+                79,
+                78
+            ],
+        }]
+    },
+    options:{
+
+    }
+})
